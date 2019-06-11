8521c3b74b89c9b274b90fea820c54322fbd16ba
windyakin <windyakin@gmail.com>
2019-06-11 22:53:45 +0900

    :tada: First revision page

---
+++ docs/shukashu.js
@@ -0,0 +1,14 @@
+(function(window, document) {
+  window.onload = function() {
+    var shukashu = ['しゅ', 'か', 'しゅー'];
+    var getRandom = function(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    };
+  
+    var result = shukashu[getRandom(shukashu.length)] + shukashu[getRandom(shukashu.length)] + shukashu[getRandom(shukashu.length)];
+    var tweetUrl = 'https://twitter.com/intent/tweet?url=http://shukasai.to/&text=' + encodeURIComponent(result);
+  
+    document.getElementById('result').textContent = result;
+    document.getElementById('tweet').setAttribute('href', tweetUrl);
+  };
+})(window, document);
